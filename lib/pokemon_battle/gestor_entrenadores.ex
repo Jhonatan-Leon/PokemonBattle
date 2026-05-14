2138b5fde7936f8f12915ffebe135e7b3d756706
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2026-05-14 18:37:17 -0500

    refactor: reorganización de la interfaz
    feat: Implementación de nuevas características para el (trainer)

--- lib/pokemon_battle/gestor_entrenadores.ex
+++ lib/pokemon_battle/gestor_entrenadores.ex
@@ -1,11 +1,7 @@
 defmodule PokemonBattle.GestorEntrenadores do
-  def login(sesion, store) do
-    # Solicitar datos para los usuarios
-    user = IO.gets("Ingrese su usuario: ") |> String.trim()
-    password = IO.gets("Ingrese su contraseña: ") |> String.trim() |> String.to_integer()
-
+  def login(trainers, store, user, password) do
     # Buscamos al entrenador
-    trainer = Enum.find(sesion, fn e -> e.usuario == user && e.clave == password end)
+    trainer = Enum.find(trainers, fn e -> e.usuario == user && e.clave == password end)
 
     if trainer do
       # menu(sesion, store)
@@ -13,37 +9,78 @@ defmodule PokemonBattle.GestorEntrenadores do
       PokemonBattle.Interfaz.menu(trainer, store)
     else
       IO.puts("Credenciales incorrectas ")
-      login(sesion, store)
+      register(trainers, store, user, password)
       # Implementar lógica para registro automatico
     end
   end
 
-  def register(sesion, store) do
-    IO.puts("Ya que no cuenta con una cuenta se realizara un registro automático ")
-
-    username = IO.gets("Ingrese su usuario: ") |> String.trim()
-    password = IO.gets("Ingrese una clave: ") |> String.trim()
-
-    new_user = Enum.find(sesion, fn e -> e.usuario == username end)
-
-    if !new_user do
-      %PokemonBattle.Trainer {
-        usuario: username,
-        clave: password
-      }
-      |>PokemonBattle.Persistencia.save_trainer()
-    else
-      IO.puts("El usuario ya existe")
-      register(sesion, store)
+  def register(trainers, store, user, password) do
+    case PokemonBattle.Trainer.nuevo(user, password) do
+      {:ok, new_trainer} ->
+        if Map.has_key?(trainers, new_trainer) do
+          {:error, "No se puede registrar"}
+        else
+          trainer = %{new_trainer | usuario: user}
+          list = Map.put(trainers, user, trainer)
+          PokemonBattle.Interfaz.menu(trainer, store)
+          {:ok, list}
+        end
     end
   end
 
-  def perfil(trainer) do
-    Enum.map(trainer, fn t ->
-      IO.puts("--Perfil de #{ t.usuario}")
-      IO.puts("Monedas: #{Enum.sum(t.monedas_actuales)}")
-      IO.puts("Sobres pendientes: #{Enum.sum(t. sobres_pendientes)}")
-      IO.puts("Pókemon en inventario: #{Enum.sum(t.inventario)}")
+  def get_trainer(trainers, usuario) do
+    case Map.get(trainers, usuario) do
+      nil -> {:error, "No encontrado"}
+      trainer -> {:ok, trainer}
+    end
+  end
+
+  def inventario(trainer) do
+    total_pokemon = length(trainer.inventario)
+    IO.puts("\n=== Inventario de #{trainer.usuario} (#{total_pokemon} Pokémon) ===")
+
+    Enum.each(trainer.inventario, fn i ->
+      movimientos =
+        i["movimientos"]
+        |> Enum.map(fn m -> "#{m["nombre"]}(#{m["poder_base"]})" end)
+        |> Enum.join(", ")
+
+      IO.puts("
+    [#{i["id"]}] #{i["especie"]}, [#{i["rareza"]}]
+    Ataque: #{i["ataque"]} | Defensa: #{i["defensa"]} | Velocidad: #{i["velocidad"]}
+    Movimientos: #{movimientos}")
     end)
+  end
+
+  def perfil(trainer) do
+    total_inventario = length(trainer.inventario)
+    IO.puts("=== Perfil de #{trainer.usuario} === ")
+    IO.puts("Monedas: #{trainer.monedas_actuales}")
+    IO.puts("Pokemon en inventario: #{total_inventario}   ")
+  end
+
+  def equipos_guardados(trainer) do
+    IO.puts("Equipos guardados: ")
+
+    Enum.map(trainer.equipos_guardados, fn e ->
+      pokemon_str = buscar_pokemon(trainer, e["pokemon_ids"], "", length(e["pokemon_ids"]))
+      IO.puts("
+      #{e["nombre"]} [#{length(e["pokemon_ids"])} / #{length(e["pokemons_ids"])}]: #{pokemon_str}
+      ")
+    end)
+
+  end
+
+  @spec buscar_pokemon(any(), list(), any(), any()) :: {:error, <<_::184>>}
+  def buscar_pokemon(_, [],_, _), do: {:error, "No hay equipos formados"}
+
+  def buscar_pokemon(trainer, [head | tail], "", num_pokemons) do
+    pokemon = Enum.find(trainer.inventario, fn p -> p["id"] == head end)
+    guardado_str = Enum.map(pokemon, fn p -> " #{p["id"]} #{p["especie"]} " end) |> Enum.join(", ")
+    buscar_pokemon(trainer.inventario, tail, guardado_str, num_pokemons - 1)
+  end
+
+  def buscar_pokemon(_, [], guardado_str, num_pokemons) when num_pokemons < 0 do
+    guardado_str
   end
 end
--- lib/pokemon_battle/modelo.ex
+++ lib/pokemon_battle/modelo.ex
@@ -5,7 +5,12 @@ end
 
 # Nombre especifico para escalar los entrenadores
 defmodule PokemonBattle.Trainer do
+  @enforce_keys [:usuario, :clave]
   defstruct [:usuario, :clave , victorias: 0, monedas_actuales: 0, monedas_acumuladas: 0, sobres_pendientes: 0, inventario: [], equipos_guardados: [], equipo: []]
+
+  def nuevo(nombre, password) do
+    {:ok, %__MODULE__{usuario: nombre, clave: password}}
+  end
 end
 
 defmodule PokemonBattle.Store do
